25907a139ade8cfc5bb374261dc86042cd6a929b
Andrea Rus <andrearus27@gmail.com>
2020-11-04 16:23:23 +0000

    wp

---
+++ fictional-university-theme/index.php
@@ -0,0 +1,12 @@
+<?php 
+  // function greet($name, $colour) {
+  //   echo "<p>Hi, my name is $name and my favourite colour is $colour </p>";
+  // }
+
+  // greet('John', 'blue');
+  // greet('Jane', 'black');
+
+  while(have_posts()) { 
+
+  }
+?>
