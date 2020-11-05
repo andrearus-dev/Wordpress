4f141b822df316176eb5f80ffdb41c01c70d696a
Andrea Rus <andrearus27@gmail.com>
2020-11-05 12:35:42 +0000

    images

--- fictional-university-theme/index.php
+++ fictional-university-theme/index.php
@@ -81,7 +81,7 @@
               </div>
             </div>
           </div>
-          <div class="hero-slider__slide" style="background-image: url(images/apples.jpg);">
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg') ?>);">
             <div class="hero-slider__interior container">
               <div class="hero-slider__overlay">
                 <h2 class="headline headline--medium t-center">An Apple a Day</h2>
@@ -90,7 +90,7 @@
               </div>
             </div>
           </div>
-          <div class="hero-slider__slide" style="background-image: url(images/bread.jpg);">
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg') ?>);">
             <div class="hero-slider__interior container">
               <div class="hero-slider__overlay">
                 <h2 class="headline headline--medium t-center">Free Food</h2>
