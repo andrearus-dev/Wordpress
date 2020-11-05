8cd07ce882301025614a3c6368e0d5833f046dba
Andrea Rus <andrearus27@gmail.com>
2020-11-05 12:02:23 +0000

    linked to font awesome

--- fictional-university-theme/functions.php
+++ fictional-university-theme/functions.php
@@ -1,6 +1,8 @@
 <?php
 
 function university_files() {
+
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('university_main_styles', get_stylesheet_uri());
 } //called wordpress function (wp_enqueue_style) and pointed towards css file we wanted to load 
 
