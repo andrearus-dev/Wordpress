d26beaaa0fe1ad52555c36da86f863095b2ae6d3
Andrea Rus <andrearus27@gmail.com>
2020-11-05 12:04:14 +0000

    linked the fonts

--- fictional-university-theme/functions.php
+++ fictional-university-theme/functions.php
@@ -2,6 +2,7 @@
 
 function university_files() {
 
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('university_main_styles', get_stylesheet_uri());
 } //called wordpress function (wp_enqueue_style) and pointed towards css file we wanted to load 
