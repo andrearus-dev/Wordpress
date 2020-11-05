d6ce09639e7a37f207070426c8ec912b1fa15958
Andrea Rus <andrearus27@gmail.com>
2020-11-05 10:05:59 +0000

    add_action()

--- fictional-university-theme/header.php
+++ fictional-university-theme/header.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <?php wp_head(); ?>
 </head>
+
 <body>
-  
+<h1>Functional University</h1>
 </body>
 </html>
 
-<h1>Title of page</h1>
